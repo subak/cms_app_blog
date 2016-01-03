fcf97fc3058315d40f210853a0ebb9ad5732cc3f
Subak Systems <info@subak.jp>
2016-01-04 06:14:58 +0900

    ファイルコピーの方法を変更

--- blog/bin/publish.sh
+++ blog/bin/publish.sh
@@ -10,7 +10,7 @@ local_context=$([ "${local}" == local ] && echo '+ {"local": true}' || echo '')
 
 filter="jq -c '. ${local_context}'"
 
-cp -rv ${APP}/public/ $([ -e "${out_dir}" ] && dirname "${out_dir}" || echo "${out_dir}")
+find ${APP}/public/* -maxdepth 1 -print0 | xargs -0 -I@ cp -rv @ "${out_dir}"
 
 for publisher in $(find ${APP}/bin/publish/*); do
   . "${publisher}"
