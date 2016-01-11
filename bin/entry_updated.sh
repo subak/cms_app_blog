02ccc52c565227205f1d0cb69bb450cebd460c85
Subak Systems <info@subak.jp>
2016-01-11 20:17:45 +0900

    gitからタイムスタンプ取得できなくなっていた

--- bin/entry_updated.sh
+++ bin/entry_updated.sh
@@ -6,10 +6,11 @@ id=$1
 
 meta=$(find content/entry -name ${id}.yml)
 file=$(find content/entry -name ${id}.adoc -o -name ${id}.md -o -name ${id}.rst)
+content_dir=$(dirname ${file})
 
 updated=$([ -n "${meta}" ] && cat ${meta} 2>/dev/null | grep updated | cut -f 2 -d' ' | sed -e "s/'//g")
 
 echo $([ -n "${updated}" ] && echo ${updated} \
-  || git log --date=iso --pretty=format:"%cd" -n 1 ${file} \
+  || echo $(cd ${content_dir} && git log --date=iso --pretty=format:"%cd" -n 1 ${file##${content_dir}/}) \
   | sed -e 's/ /T/' | sed -e 's/ //' | egrep . \
   || date --iso-8601=minutes)
